a3ba7953d181f1b35edb2f4bcab82db266d61664
Shifat Sahariar Bhuiyan <shifat.sahariar@gmail.com>
2022-06-11 14:48:20 +0200

    Hotel Management System

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -3,21 +3,29 @@ package model;
 import java.util.regex.Pattern;
 
 public class Customer {
-    private String firstName ;
-    private String lastName;
-    private String email;
+    private final String firstName ;
+    private final String lastName;
+    private final String email;
     public Customer(String firstName, String lastName, String validEmail) {
 
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = validEmail;
+        this.email = emailValidation(validEmail);
     }
 
 
     public  String getEmail(){
         return  this.email;
     }
+    private String emailValidation(String email) {
+        String EMAIL_REGEX = "^(.+)@(.+).com$";
+        if (!email.matches(EMAIL_REGEX)) {
+            throw new IllegalArgumentException("Email is not valid ");
+        } else {
+            return email;
+        }
 
+    }
     @Override
     public String toString() {
         return "Customer Name : " + firstName +"  "+ lastName + " \n Email " + email;
